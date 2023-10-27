5d41058f1a4e163e976e3b4a12d15efa4f7b7c14
airhac <oooh32141@gmail.com>
2023-10-27 17:53:36 +0900

    재 설정 - :sparkles:Feat : 회원가입 기능 추가 #3   (계정, 이메일, 비밀번호로 회원 가입)   - :sparkles:Feat :회원가입 제약조건 2이상 추가 #3     구현한 제약 조건       - 숫자, 문자, 특수문자 중 2가지 이상을 포함       - 3회 이상 연속되는 문자 사용 불가       - 개인 정보와 비밀번호가 유사한지 체크       - 비밀 번호가 최소 10자 이상이 되어야한다       - 흔한 비밀번호는 사용할 수 없다       - 전부 숫자인 비밀번호를 사용하지 못하게 한다. - :sparkles:Feat : 로그인 기능 추가 #5

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
-from accounts import views
+from django.urls import include, path
+from . import views
+from .views import CustomLoginView
 
-app_name = "accounts"
+app_name = "auth"
 # base_url: v1/accounts/
 
 urlpatterns = [
-    # 
+    path('login/', CustomLoginView.as_view(), name='custom-login'),
+    path('', include('dj_rest_auth.urls'), name='dj_rest_auth'),
+    path('registration/', include('dj_rest_auth.registration.urls'), name='registration'),
 ]
