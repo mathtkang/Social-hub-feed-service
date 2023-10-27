7f70aa244c621f8e06f5d00c9f2683e62e61862e
tneoeo2 <tneoeo2@gmail.com>
2023-10-27 20:31:39 +0900

    :recycle:Refact : 코드 리팩토링
    ################
    - views.py 예외처리 추가
    - UserSerializer 변경 -> 이메일 변경방지
    
    ################
    #4

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,18 +1,14 @@
 from rest_framework import serializers
 from .models import User
 
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'auth_code']
-        extra_kwargs = {
-            'email': {'read_only': True},  
-        }
-
-class UserCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password', 'auth_code']
-        extra_kwargs = {
-            'auth_code': {'read_only': True},  
-        }
+        read_only_fields = ['email']
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ import string
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
-# from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
 from django.core.mail import EmailMessage
@@ -14,14 +14,10 @@ from .serializers import UserSerializer, UserCreateSerializer
 
 
 # Create your views here.
-'''
-    username(해시태그)
-    email 
-    입력시 유저 생성 후 이메일 보냄
-'''
-#? /auth/registration/
+#! /auth/registration/  -> 테스트용
 class UserCreateView(CreateAPIView):
     serializer_class = UserCreateSerializer
+    
     def perform_create(self, serializer):
         data = serializer.validated_data
         username = data['username']
@@ -43,18 +39,18 @@ class UserCreateView(CreateAPIView):
         }
         
         print("response_data: ", response_data)
-        # send_mail(subject, message, from_email)    #* 이메일 발송은 생략
+        # send_mail(subject, message, from_email)    #* 이메일 발송은 생략 -> 회원생성과 동시에 이메일 발송할 경우
         return Response(response_data, status = status.HTTP_201_CREATED)
 
-#? /auth/code/
+#? /auth/code/ : 가입승인코드 일치 확인
 class UserActivationView(APIView): # 가입승인코드 확인
     serializer_class = UserSerializer
     def post(self, request):
         user =  request.data.get('username')
         auth_code = request.data.get('auth_code')  
-        # email = request.data.get('email')
-        user = User.objects.get(username=user)
+        # email = request.data.get('email')   # 이메일 입력도 필요할 경우 사용
         try:
+            user = User.objects.get(username=user)
             if  user.auth_code == auth_code:
                 user.auth_code = None   # 인증된 유저는 인증코드 삭제
                 user.is_active = True
@@ -65,32 +61,37 @@ class UserActivationView(APIView): # 가입승인코드 확인
         except User.DoesNotExist:
             return Response({'message': '사용자를 찾을 수 없습니다.'}, status=status.HTTP_400_BAD_REQUEST)
         
-
+        
+#? /auth/<username>/ : username로 인증코드 메일전송 -> 이메일 재발송이 필요한경우
 class SendEmailView(RetrieveAPIView):
     permission_classes = [AllowAny]
     serializer = UserSerializer
     lookup_field = 'username'
 
     def get(self, request, username):
-        user = self.get_queryset()
-        # smtp사용해서 메일보내는 코드
-        try:
-            subject = "메일제목"
-            email = user.email
-            auth_code = user.auth_code
-            to = [email]
-            message = auth_code # 메일 내용  #최초 회원가입시도시 생성된 인증코드
-            
-            #* EmailMessage(subject=subject, body=message, to=to).send() # 메일 보내기
-            # Response 확인용
-            response_data = {
-                'subject': subject,
-                'message': message,
-                'to': to,
-            }
-            return Response({'메일 전송 완료':response_data}, status=status.HTTP_200_OK)
-        except Exception as e:    
-            return Response({'message': '사용자를 찾을 수 없습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        # user = self.get_queryset()
+        user = get_object_or_404(User, username=username)
+        if user.auth_code:
+            try: 
+                subject = "메일제목"
+                email = user.email
+                auth_code = user.auth_code
+                to = [email]
+                message = auth_code # 메일 내용  #최초 회원가입시도시 생성된 인증코드
+                
+                #* EmailMessage(subject=subject, body=message, to=to).send() # 메일 보내기
+                
+                # Response 확인용
+                response_data = {
+                    'subject': subject,
+                    'message': message,
+                    'to': to,
+                }
+                return Response({'메일 전송 완료':response_data}, status=status.HTTP_200_OK)
+            except Exception as e:
+                return Response({'message': '메일 전송 중 오류가 발생했습니다.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response({'message': '사용자를 찾을 수 없거나 인증 코드가 존재하지 않습니다.'}, status=status.HTTP_404_NOT_FOUND)
     
     def get_queryset(self):
         username = self.kwargs['username']
