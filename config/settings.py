966b29ec52d5ab941b4b5fd9841fa74813d95931
airhac <oooh32141@gmail.com>
2023-10-29 03:11:29 +0900

    :recycle:Refact : 코드 리팩토링 #8 - 관련 db 및 환경설정 필요없는 부분 제거(중요x)

--- config/settings.py
+++ config/settings.py
@@ -195,9 +195,6 @@ AUTH_PASSWORD_VALIDATORS = [
         },
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
