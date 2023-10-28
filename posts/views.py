22d2e45b0c2d3b05f432675220dd3790911c1b1c
airhac <oooh32141@gmail.com>
2023-10-29 03:09:55 +0900

    :sparkles:Feat : 게시물 상세페이지 추가 및 게시물 조회수 count 기능 추가 #8

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from .models import Posts
+from .serializers import PostSerializer
+from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+# Blog의 detail을 보여주는 역할
+class PostsDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Posts.objects.all()
+    serializer_class = PostSerializer
+    
+    # permission_classes = [IsAuthenticated, ]
+    permission_classes = [AllowAny, ]
+    
+    def get(self, request, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        instance = get_object_or_404(Posts, id=pk)
+        #Posts에서 가지고온 query에서 해당 게시물 조회수를 가지고 옵니다.
+        view_count = self.queryset.values().get(id=pk)['view_count']
+        #get요청이 들어 올때 마다 해달 view_count를 +1합니다.
+        view_count+=1
+        instance.view_count = view_count
+        instance.save()
+        
+        return self.retrieve(request, *args, **kwargs)
