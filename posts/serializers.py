b6df0b504ed862d1a5ac4354dd280a426db33e4e
YuJinsoo <realaqua23@gmail.com>
2023-10-28 23:03:50 +0900

    :sparkles:Feat : post, hashtag serializer 개발 #7

---
+++ posts/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from posts.models import Posts, HashTags
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Posts
+        fields = '__all__'
+
+
+class HashTagSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = HashTags
+        fields = '__all__'
+        
